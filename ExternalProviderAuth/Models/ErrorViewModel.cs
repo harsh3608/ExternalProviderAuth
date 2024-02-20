ada284be91ef6799916da3af3923a8853c590de8
Harsh Patel <harsh3608@gmail.com>
2024-02-20 10:32:33 +0530

    Add project files.

---
+++ ExternalProviderAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExternalProviderAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
